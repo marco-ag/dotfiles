2cd27237b9b29cac88eb33d48f552c825479dd8d
marco-ag <ag@archlinux.com>
2025-10-28 12:19:40 -0600

    first commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/hypr ~/.config/hypr
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/swaync ~/.config/swaync
+ln -sf ~/dotfiles/waybar ~/.config/waybar
